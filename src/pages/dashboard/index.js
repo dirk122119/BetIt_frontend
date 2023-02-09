60beb9fc9dbd787bfbd67ac8c891ec48621cf586
dirk122119 <dirk122119@gmail.com>
2023-02-09 16:36:49 +0800

    return default

---
+++ src/pages/dashboard/index.js
@@ -0,0 +1,10 @@
+import Layout from '@/components/layout'
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <Layout>
+      {/* <Component {...pageProps} /> */}
+      <h1>HI</h1>
+    </Layout>
+  )
+}
